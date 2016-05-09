64c23fc8485d71735bfe4388773b84923843e957
Alex <digwnews@gmail.com>
2016-05-09 15:43:19 +0300

    settings upload fix

--- goodwork/settings.py
+++ goodwork/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/home/alex/goodwork/uploads/'
+MEDIA_ROOT = 'uploads/'
 
 
 LOGIN_URL = '/signin/'
